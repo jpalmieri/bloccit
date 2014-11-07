9cd4e92891108aefdecab44682a64e4da0ddee38
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-07 23:18:18 +0000

    Added validation, created partial for new and edit posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def my_email
     "jpalmieri@lumoslabs.com"
   end
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
