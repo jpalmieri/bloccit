42b407abc198a4ed51e2018945c23911b6a32968
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-13 05:16:43 +0000

    added validation and flash errors for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
     if @comment.save
       flash[:notice] = "Comment was saved."
       redirect_to [@topic, @post]
+    elsif @comment.errors.any?
+      flash[:error] = "Error#{'s' if @comment.errors.count > 1} saving comment: 
+      #{@comment.errors.full_messages.join(', ')}"
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
       redirect_to [@topic, @post]
