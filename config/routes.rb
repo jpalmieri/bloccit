ad21d0d6a5b0d714b0fae2c0f5021d7a909f31b3
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-13 04:15:02 +0000

    removed unneeded comment#new route. Fixed wrong render on comment error (should go back to post rather than new comment view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Bloccit::Application.routes.draw do
 
    resources :topics do
     resources :posts, except: [:index] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:create]
     end
    end
 
