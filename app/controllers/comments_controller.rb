ef2c2a057ed09e1f9f891cd44aa48e3481a20047
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-13 05:41:14 +0000

    added comment policy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new
+    authorize @comment
   end
 
   def create
@@ -10,6 +11,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(params.require(:comment).permit(:body))
     @comment.post = @post
+    authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
       redirect_to [@topic, @post]
