affcfd25670a66a2c861456ac7b32561d86628db
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-25 23:12:07 +0000

    added spec for signed in user visiting own profile, passing

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
+  
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A Comment")
+    @comment = @post.comments.new(user: @user, body: "A Comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -24,4 +27,23 @@ describe "Visiting profiles" do
     end
 
   end
+
+  describe "signed in user visits their own profile" do
+
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+
+  end
+
+  Warden.test_reset!
 end
