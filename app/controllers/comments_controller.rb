0624dbd1a171c18ecd69420edbbb1c0e5eff78b6
jpalmieri <jpalmieri@lumoslabs.com>
2014-11-29 00:48:16 +0000

    added ujs for comment creation. removed unused post views (I must have missed deleting them before)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,24 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
-    @comment = current_user.comments.build(params.require(:comment).permit(:body))
+    @comments = @post.comments
+
+    @comment = current_user.comments.build( comment_params )
     @comment.post = @post
+
     authorize @comment
+
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post]
     elsif @comment.errors.any?
       flash[:error] = "Error#{'s' if @comment.errors.count > 1} saving comment: 
       #{@comment.errors.full_messages.join(', ')}"
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
@@ -36,6 +40,12 @@ class CommentsController < ApplicationController
     respond_with(@comment) do |format|
       format.html { redirect_to [@post.topic, @post] }
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 
 end
